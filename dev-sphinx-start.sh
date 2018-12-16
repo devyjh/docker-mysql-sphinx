9863b92d4ab49b01742ddf7dff3f675c46ae65c6
arc_kjh <arc_kjh@mustit.co.kr>
2018-12-16 13:13:41 +0000

    sphinx script bug fix

--- dev-sphinx-start.sh
+++ dev-sphinx-start.sh
@@ -12,8 +12,8 @@ SPHINX_DATA="/home/$USER/sphinx"
 mkdir -p $SPHINX_DATA
 docker pull $SPHINX_IMAGE
 
-docker run --name sphinx 
-            -v $SPHINX_DATA:/var/idx/sphinx 
-            -v $SPHINX_CONF:/usr/local/etc
-            -p 9306:9306 -p 9312:9312 
+docker run --name sphinx \
+            -v $SPHINX_DATA:/var/idx/sphinx \
+            -v $SPHINX_CONF:/usr/local/etc \
+            --net host \
             -d $SPHINX_IMAGE
